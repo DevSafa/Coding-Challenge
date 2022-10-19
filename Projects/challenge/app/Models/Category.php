961c934e85b640b3de72566210e6ea988df39758
Safa Barka <software.dev.safa@gmail.com>
2022-10-19 20:31:15 +0100

    correct the mistake , Categroy has children and parent NOTT PRODUCT

--- Projects/challenge/app/Models/Category.php
+++ Projects/challenge/app/Models/Category.php
@@ -21,4 +21,25 @@ class Category extends Model
     {
         return $this->belongsToMany(Product::class);
     }
+
+    
+    /* define the self relationship  in Category table  => oneToMany self relationship */
+    /**
+     * A one-to-many relationship is used to define relationships where a single 
+     * model is the parent to one or more child models 
+     * 
+     */
+    /** every category has children */
+  
+    public function children()
+    {
+        return $this->hasMany(Category::class , 'parent_id');
+    }
+
+    /* every category has a parent : single parent */
+    /* One To Many (Inverse) / Belongs To */
+    public function parent()
+    {
+        return $this->belongsTo(Category::class ,'parent_id');
+    }
 }
