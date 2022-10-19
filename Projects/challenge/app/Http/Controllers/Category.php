01d981905113fc6933807d9091e8f5baf887f834
Safa Barka <software.dev.safa@gmail.com>
2022-10-19 22:24:04 +0100

    user container Service to inject my dependency inside controler + define controller methods

--- Projects/challenge/app/Http/Controllers/Category.php
+++ Projects/challenge/app/Http/Controllers/Category.php
@@ -1,9 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Repositories\Category\CategoryRepositoryInterface;
 class Category extends Controller
 {
     /** define constructor to inject CategoryRepository interface in Category Controller */
     /** need to add CategoryRepository interface as provider */
+    public function __construct(CategoryRepositoryInterface $categoryRepositoryInterface)
+    {
+        $this->categoryRepository = $categoryRepositoryInterface;
+    }
+
+    public function categories()
+    {
+        return $this->categoryRepository->categories();
+    }
+
+ 
+    public function parents()
+    {
+        return  $this->categoryRepository->parents();
+    }
+
+
+    public function childs($id)
+    {
+        return $this->categoryRepository->childs($id);
+    }
+
+
+    public function parent($id)
+    {
+        return $this->categoryRepository->parent($id);
+    }
+
+   
+    public function products($id)
+    {
+        return $this->categoryRepository->products($id);
+    }
 }
