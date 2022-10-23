dbb25f6fe737e3920bc64702b0bfc6c66c039212
Safa Barka <software.dev.safa@gmail.com>
2022-10-23 07:08:31 +0100

    format files

--- Projects/challenge/resources/js/components/CategoryComponent.vue
+++ Projects/challenge/resources/js/components/CategoryComponent.vue
@@ -1,23 +1,16 @@
 <template>
-<div class="container">
+    <div class="container">
         <div class="category">
             <h1>Categories</h1>
-            <categoryItem 
-                v-for="(item, index) in categoryTree"
-                    :key="index"
-                    :name="item.name"
-                    :space="0"
-                    :data="item.children"
-                    :id="item.id"
-                    v-on:callChange="changeFromChild"
-             
-            />
+            <categoryItem v-for="(item, index) in categoryTree" :key="index" :name="item.name" :space="0"
+                :data="item.children" :id="item.id" v-on:callChange="changeFromChild" />
         </div>
         <div class="Products">
+
             <h1>Products</h1>
-            <productComponent
-                :products=this.products
-            />
+            <productComponent :products=this.products />
+
+
         </div>
     </div>
 </template>
@@ -28,45 +21,51 @@ import axios from 'axios';
 import categoryItem from './SubCategoryComponent.vue'
 import productComponent from './ProductComponent.vue'
 
-    export default {
-        name : "recursive-category",
-        created() {
-            this.fetchData();
-        },
-        methods : {
-            fetchData(){
-                axios.get("/categories")
+
+export default {
+    name: "recursive-category",
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            axios.get("/categories")
                 .then(res => {
                     this.categoryTree = res.data;
                 })
 
-                axios.get("/products")
-                    .then(res => {
-                        this.products = res.data;
-                    })
-                
-            },
+            axios.get("/products")
+                .then(res => {
+                    this.products = res.data;
+                })
 
-            changeFromChild(id)
-            {
-               console.log("hello from Category",id);
-                axios.get(`/category/products/${id}`)
-                    .then(res => {
-                        this.products = res.data;
-                    })
-            }
         },
-        data () {
-            return {
-                categoryTree : [],
-                products : [],
-                which : ""
-            }
+
+        changeFromChild(id) {
+            console.log("hello from Category", id);
+            axios.get(`/category/products/${id}`)
+                .then(res => {
+                    this.products = res.data;
+                })
+
+
+
         },
-        components : {
-            categoryItem,
-            productComponent
-}
+
+    },
+    data() {
+        return {
+            categoryTree: [],
+            products: [],
+            which: "",
+
+        }
+    },
+    components: {
+        categoryItem,
+        productComponent,
+
     }
+}
 </script>
 
--- Projects/challenge/resources/js/components/SubCategoryComponent.vue
+++ Projects/challenge/resources/js/components/SubCategoryComponent.vue
@@ -1,41 +1,24 @@
 <template>
-        <div class="category-item">
-            <div class="name"
-            @click="showcategory()"
-                :style="{
-                    paddingLeft: space * 30 + 10 + 'px',
-                }"
-            >
+    <div class="category-item">
+        <div class="name" @click="showcategory()" :style="{
+            paddingLeft: space * 30 + 10 + 'px',
+        }">
             <div>
                 <button type="button" class="btn btn-outline-dark">{{name}}</button>
             </div>
-
-            <div v-if="data">
-                <i class="material-icons expand ">expand_more</i>
-            </div>
-            
-            </div>
-        <div class="item-container"
-            v-show="showChildren"
-        >
-                <category-item
-                v-for="(item, index) in data"
-                    :key="index"
-                    :name="item.name"
-                    :space=" space + 1 "
-                    :data="item.children"
-                    :id="item.id"
-                    v-on:callChange="changeFromChild"
-                />
-            </div>
         </div>
+        <div class="item-container" v-show="showChildren">
+            <category-item v-for="(item, index) in data" :key="index" :name="item.name" :space=" space + 1 "
+                :data="item.children" :id="item.id" v-on:callChange="changeFromChild" />
+        </div>
+    </div>
 
 </template>
 
 <script>
 
 
-    export default {
+export default {
     name: "category-item",
     data: () => ({
         showChildren: false,
@@ -43,8 +26,8 @@
         categories: []
     }),
     props: {
-        id : {
-            type :Number
+        id: {
+            type: Number
         },
         name: {
             type: String,
@@ -58,44 +41,43 @@
         data: {
             type: Array
         },
-      
+
     },
     methods: {
-        changeFromChild($id)
-        {
-            this.$emit('callChange',$id)
-        
+        changeFromChild($id) {
+            this.$emit('callChange', $id)
+
         },
         showcategory() {
             this.showChildren = !this.showChildren;
-            this.$emit('callChange',this.id);
+            this.$emit('callChange', this.id);
         }
 
-       
-  
+
+
     },
 
- 
+
 }
 </script>
 
 <style lang="scss" scoped>
-    .category-item {
-        position : relative;
-        .name  {
-            width : 100%;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            white-space:nowrap;
-            user-select: none;
-            height: 50px;
-            padding:  0 20px;
-            box-sizing: border-box;
+.category-item {
+    position: relative;
 
-            
-        }
-     
+    .name {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        white-space: nowrap;
+        user-select: none;
+        height: 50px;
+        padding: 0 20px;
+        box-sizing: border-box;
+
+
     }
-  
+
+}
 </style>
