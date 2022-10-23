5de2af84e3b43d2b5a0a88c496606eb1ac41671f
Safa Barka <software.dev.safa@gmail.com>
2022-10-23 04:21:50 +0100

    passing data from recursive children to parent using emit function != props for passing data from parent to children

--- Projects/challenge/resources/js/components/CategoryComponent.vue
+++ Projects/challenge/resources/js/components/CategoryComponent.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="container">
+<div class="container">
         <div class="category">
             <h1>Categories</h1>
-            <categoryItem
+            <categoryItem 
                 v-for="(item, index) in categoryTree"
                     :key="index"
                     :name="item.name"
                     :space="0"
                     :data="item.children"
+                    :id="item.id"
+                    v-on:callChange="changeFromChild"
+             
             />
         </div>
-        <div class="products">
+        <div class="Products">
             <h1>Products</h1>
+            <productComponent
+            />
         </div>
     </div>
-  
- 
 </template>
 
 
 <script>
 import axios from 'axios';
 import categoryItem from './SubCategoryComponent.vue'
+import productComponent from './ProductComponent.vue'
     export default {
         name : "recursive-category",
         created() {
@@ -33,6 +37,10 @@ import categoryItem from './SubCategoryComponent.vue'
                 .then(res => {
                     this.categoryTree = res.data;
                 })
+            },
+            changeFromChild($id)
+            {
+                console.log("hello from Category",$id);
             }
         },
         data () {
@@ -41,8 +49,9 @@ import categoryItem from './SubCategoryComponent.vue'
             }
         },
         components : {
-            categoryItem
-        }
+            categoryItem,
+            productComponent
+}
     }
 </script>
 
--- Projects/challenge/resources/js/components/SubCategoryComponent.vue
+++ Projects/challenge/resources/js/components/SubCategoryComponent.vue
@@ -1,64 +1,82 @@
 <template>
-    <div class="category-item">
-        <div class="name"
-        @click="showcategory()"
-            :style="{
-                paddingLeft: space * 30 + 10 + 'px',
-            }"
+        <div class="category-item">
+            <div class="name"
+            @click="showcategory()"
+                :style="{
+                    paddingLeft: space * 30 + 10 + 'px',
+                }"
+            >
+            <div>
+                <button type="button" class="btn btn-outline-dark">{{name}}</button>
+            </div>
+
+            <div v-if="data">
+                <i class="material-icons expand ">expand_more</i>
+            </div>
+            
+            </div>
+        <div class="item-container"
+            v-show="showChildren"
         >
-        <div>
-            <button type="button" class="btn btn-outline-dark">{{name}}</button>
+                <category-item
+                v-for="(item, index) in data"
+                    :key="index"
+                    :name="item.name"
+                    :space=" space + 1 "
+                    :data="item.children"
+                    :id="item.id"
+                    v-on:callChange="changeFromChild"
+                />
+            </div>
         </div>
 
-        <div v-if="data">
-             <i class="material-icons expand ">expand_more</i>
-        </div>
-           
-        </div>
-     <div class="item-container"
-        v-show="showChildren"
-    >
-             <category-item
-            v-for="(item, index) in data"
-                :key="index"
-                :name="item.name"
-                :space=" space + 1 "
-                :data="item.children"
-            />
-        </div>
-    </div>
 </template>
 
 <script>
 
+
     export default {
-        name : "category-item",
-        data: () => ({
-            showChildren:false,
-            expanded: false,
-            categories : []
-        }),
-        props : {
-            name : {
-                type: String,
-                required : true
-            },
-            // space will be user to compute teh offset of children items
-            space : {
-                type : Number,
-                required : true
-            },
-            data : {
-                type : Array
-            }
+    name: "category-item",
+    data: () => ({
+        showChildren: false,
+        expanded: false,
+        categories: []
+    }),
+    props: {
+        id : {
+            type :Number
         },
-        methods :{
-            showcategory() {
-                this.showChildren = !this.showChildren;
-            },
+        name: {
+            type: String,
+            required: true
+        },
+        // space will be user to compute teh offset of children items
+        space: {
+            type: Number,
+            required: true
+        },
+        data: {
+            type: Array
+        },
+      
+    },
+    methods: {
+        changeFromChild($id)
+        {
+            this.$emit('callChange',$id)
         
+        },
+        showcategory() {
+            this.showChildren = !this.showChildren;
+            this.$emit('callChange',this.id);
         }
-    }
+
+       
+  
+    },
+
+ 
+}
 </script>
 
 <style lang="scss" scoped>
