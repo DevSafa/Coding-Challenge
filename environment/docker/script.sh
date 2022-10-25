b0d80e27234b07a5024a2aba6cd90b4415426708
Safa Barka <software.dev.safa@gmail.com>
2022-10-25 01:01:37 +0100

    change the host in .env + seed categories in database

--- environment/docker/script.sh
+++ environment/docker/script.sh
@@ -2,5 +2,7 @@
 docker-compose down
 docker-compose up -d 
 sleep 10
+docker-compose exec laravel-app composer install 
+docker-compose exec laravel-app npm install 
 docker-compose exec laravel-app php artisan migrate
-docker-compose exec laravel-app php artisan db:seed
+docker-compose exec laravel-app php artisan db:seed CategorySeeder
