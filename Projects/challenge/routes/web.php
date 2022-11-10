5f84a6693fcf63392feb8a947d870bfb04124488
Safa Barka <software.dev.safa@gmail.com>
2022-11-11 00:18:31 +0100

    routes to get and post data

--- Projects/challenge/routes/web.php
+++ Projects/challenge/routes/web.php
@@ -14,9 +14,10 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/categories',[CategoryController::class,"index"]);
-Route::post('/categories',[ProductController::class,"store"]);
-
+Route::post('/products',[ProductController::class,"store"]);
+Route::get('/products',[ProductController::class,"index"]);
 
 
