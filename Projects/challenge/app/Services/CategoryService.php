3f7657c5524e8997e6f0ad799ffc520653bc68f0
Safa Barka <software.dev.safa@gmail.com>
2022-11-11 00:12:37 +0100

    Services clean code and refactoring : CategoryService + ProductService

--- Projects/challenge/app/Services/CategoryService.php
+++ Projects/challenge/app/Services/CategoryService.php
@@ -3,17 +3,29 @@ namespace App\Services;
 
 use App\Interfaces\CategoryServiceInterface;
 use App\Interfaces\CategoryRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class CategoryService implements CategoryServiceInterface {
-
 	private $categoryRepository;
 
+	/**
+	 * Create a new CategoryService instance.
+	 *
+	 * @param  CategoryRepository  $categoryRepository
+	 * 
+	 * @return void
+	*/
 	public function __construct(CategoryRepositoryInterface $categoryRepository)
 	{
 		$this->categoryRepository = $categoryRepository;
 	}
 
-	public function index() : array
+	/**
+	 * get all categories
+	 *
+	 * @return  Illuminate\Database\Eloquent\Collection
+	*/
+	public function index() : EloquentCollection
 	{
 		return $this->categoryRepository->index();
 	}
--- Projects/challenge/app/Services/ProductService.php
+++ Projects/challenge/app/Services/ProductService.php
@@ -1,15 +1,105 @@
 <?php
+
 namespace App\Services;
 
 use App\Interfaces\ProductServiceInterface;
 use App\Interfaces\ProductRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection as SupportCollection;
 
 class ProductService implements ProductServiceInterface {
 
-	private $productRepository;
+	/**
+	 * The product repository instance.
+	*/
+	protected $productRepository;
 
+	/**
+	 *  Illuminate\Support\Collection instance.
+	*/
+	protected $values;
+
+	/**
+	 * Create a new ProductService instance.
+	 *
+	 * @param  ProductRepository  $productRepository
+	 * 
+	 * @return void
+	*/
 	public function __construct(ProductRepositoryInterface $productRepository)
 	{
 		$this->productRepository = $productRepository;
+	}
+
+	/**
+	 * get all products
+	 *
+	 * @return  Illuminate\Database\Eloquent\Collection
+	*/
+	public function index() : EloquentCollection
+	{
+		return $this->productRepository->index();
+	}
+
+	/**
+	 * get image extension from image object from  SupportCollection instance
+	 *
+	 * @return  string
+	*/
+	protected function getImageExtension() : string
+	{
+		
+		return $this->values['image']->extension();
+	}
+
+	/**
+	 * get image name from image object from  Supportcollection instance
+	 *
+	 * @return  string
+	*/
+	protected function getImageName() : string
+	{
+		return $this->values['name'];
+	}
+
+	/**
+	 * generate a unique image name 
+	 *
+	 * @return  string
+	*/
+	protected function generateImageName() : string
+	{
+		return uniqid() . '-' .$this->getImageName(). '.' . $this->getImageExtension();
+	}
+
+	/**
+	 * getPrice of an image from SupportCollection instance and cast it to float
+	 *
+	 * @return  float
+	*/
+	protected function getPrice() : float
+	{
+		return (float)($this->values['price']);
+	}
+
+	/**
+	 * the final data to pass to database Repository
+	 *
+	 * @param  Illuminate\Support\Collection  $values
+	 * 
+	 * @return  void
+	*/
+	public function store(SupportCollection $values) 
+	{	
+		$this->values = $values;
+
+		$collection = collect([
+			'name' => $values['name'],
+			'description' => $values['description'],
+			'price' => $this->getPrice(),
+			'image' =>$this->generateImageName()
+
+		]);
+		$this->productRepository->store(($collection));
 	}
 }
