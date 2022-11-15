68d0e84e3fc72a2794a5816874c79150fb9d56e7
sbarkadev <software.dev.safa@gmail.com>
2022-11-15 23:25:54 +0100

    create a command line to create  a product

--- Projects/challenge/app/Console/Commands/CreateProduct.php
+++ Projects/challenge/app/Console/Commands/CreateProduct.php
@@ -2,7 +2,16 @@
 
 namespace App\Console\Commands;
 
+
+use App\Interfaces\CliServiceInterface;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Collection;
+use Illuminate\Http\UploadedFile;
+use App\Http\Requests\CreateProductRequest;
+use Illuminate\Http\Exception;
+use Illuminate\Support\Facades\Storage;
+
 
 class CreateProduct extends Command
 {
@@ -11,24 +20,133 @@ class CreateProduct extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'product:create';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create a product from command line';
+
+    /**
+     * a collection instance to store validated data
+     * 
+     * @var Illuminate\Support\Collection
+     */
+    protected $validData;
+
+    /**
+     * a collection instance to store errors 
+     * 
+     * @var App\Interfaces\CliServiceInterface
+     */
+    protected $errors;
+
+    /**
+     * The cliService instance.
+     * 
+     * @var App\Interfaces\CliServiceInterface
+    */
+	protected $cliService;
 
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param App\Interfaces\CliServiceInterface $cliService
+     * 
+     * @return void 
      */
-    public function __construct()
+    public function __construct(CliServiceInterface $cliService)
     {
         parent::__construct();
+        $this->cliService = $cliService;
+        $this->validData =collect();
+        $this->errors =collect();
+
     }
+
+    /**
+     * push errors to collection 
+     * 
+     * @param string $fiels 
+     * @param array  $errors
+     * 
+     * @return void
+     */
+    protected function addError(string $field , array $errors) : void
+    {
+        for($i = 0 ; $i < count($errors) ; $i++)
+        {
+            $this->errors->push($errors[$i]);
+        }
+    }
+
+    /**
+     * display collection of errors to the user
+     * 
+     * @return void 
+     */
+    protected function displayErrors() : void
+    {
+        for($i = 0 ; $i < count($this->errors) ; $i++)
+        {
+            $this->line("<bg=red>".$this->errors[$i]."</>");
+        }
+    }
+
+    /**
+     * validate different data entered by the user
+     * 
+     * @param string $field
+     * 
+     * @param string $value
+     * 
+     * @param  array $rules
+     * 
+     * @return void 
+     */
+    public function validate(string $field  , string $value = null,array $rules) : array
+    {
+        $validator = Validator::make(
+            [ $field => $value],
+            [ $field => $rules]
+        );
+        if($validator->fails())
+            $this->addError($field,$validator->errors()->all());
+        $this->validData->put($field,$value);
+        return [];
+    }
+
+    /**
+     * ask user for input data.
+     * 
+     * do validation
+     * 
+     * @return void
+     */
+    protected function askAndvalidateInput() : void 
+    {
+        $data = $this->ask('Product\'s name');
+        $this->validate("name",$data,['required','string','unique:products']);
+      
+        
+        $data = $this->ask('Describe your product ');
+        $this->validate("description",$data,['required','string','max:1000']);
+    
+    
+        $data = $this->ask('Product\'s Price ');
+        $this->validate("price",$data,['required','numeric','gt:0']);
+     
+        
+        $data = $this->ask('Product\'s Category');
+        $this->validate("category",$data,['required','exists:categories,name']);
+    
+
+        $data = $this->ask('image url');
+        $this->validate("url",$data,['required','url']);
+    }
+  
 
     /**
      * Execute the console command.
@@ -37,6 +155,19 @@ class CreateProduct extends Command
      */
     public function handle()
     {
-        return 0;
+        $this->askAndvalidateInput();
+
+        if (count($this->errors))
+            return $this->displayErrors($this->errors);
+        /** create an uploaded image  */
+        $image = $this->cliService->getFile($this->validData['url']);
+        $file = $this->cliService->getUploadFile($image, $this->validData['name']);
+        $this->validData->put("image",$file);
+
+        $this->validData->forget('url');
+        $categories =  $this->cliService->getCategoryService()->getCategories($this->validData['category']);
+		$product = $this->cliService->getProductService()->storeProduct($this->validData->put('categories',$categories));
+
     }
+
 }
