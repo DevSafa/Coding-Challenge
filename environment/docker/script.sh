21a211ffe05443c8ce01ac7d546e91bd9f9b384f
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 16:35:54 +0100

    coding style seeders ...

--- environment/docker/script.sh
+++ environment/docker/script.sh
@@ -2,7 +2,11 @@
 docker-compose build laravel-app
 docker-compose up -d 
 sleep 10
-docker-compose exec laravel-app composer install
+docker-compose exec laravel-app composer install 
+docker-compose exec laravel-app npm install 
+docker-compose exec laravel-app npm run production
 docker-compose exec laravel-app php artisan migrate
-docker-compose exec laravel-app php artisan db:seed  
 docker-compose exec laravel-app php artisan storage:link
+docker-compose exec laravel-app php artisan db:seed ProductSeeder
+docker-compose exec laravel-app php artisan db:seed CategorySeeder
+docker-compose exec laravel-app php artisan db:seed ProductCategorySeeder
