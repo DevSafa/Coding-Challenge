bd7e626fead6b21007db837b5b2f898401dfdf70
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 15:42:12 +0100

    coding style for CreateProduct commadn

--- Projects/challenge/app/Console/Commands/CreateProduct.php
+++ Projects/challenge/app/Console/Commands/CreateProduct.php
@@ -2,46 +2,36 @@
 
 namespace App\Console\Commands;
 
-
 use App\Interfaces\CliServiceInterface;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Collection;
-use Illuminate\Http\UploadedFile;
-use App\Http\Requests\CreateProductRequest;
-use Illuminate\Http\Exception;
-use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Support\Collection as SupportCollection;
 
 class CreateProduct extends Command
 {
     /**
      * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'product:create';
 
     /**
      * The console command description.
-     *
      * @var string
      */
     protected $description = 'Create a product from command line';
 
     /**
      * a collection instance to store validated data
-     * 
-     * @var Illuminate\Support\Collection
+     * @var SupportCollection
      */
-    protected $validData;
+    protected $validData = null;
 
     /**
      * a collection instance to store errors 
-     * 
-     * @var App\Interfaces\CliServiceInterface
+     *@var SupportCollection
      */
-    protected $errors;
+    protected $errors = null ;
 
     /**
      * The cliService instance.
@@ -52,7 +42,6 @@ class CreateProduct extends Command
 
     /**
      * Create a new command instance.
-     *
      * @param App\Interfaces\CliServiceInterface $cliService
      * 
      * @return void 
@@ -69,28 +58,23 @@ class CreateProduct extends Command
     /**
      * push errors to collection 
      * 
-     * @param string $fiels 
-     * @param array  $errors
-     * 
+     * @param array errors 
      * @return void
      */
-    protected function addError(string $field , array $errors) : void
+    protected function addError(array $errors): void
     {
-        for($i = 0 ; $i < count($errors) ; $i++)
-        {
+        for ($i = 0 ; $i < count($errors) ; $i++) {
             $this->errors->push($errors[$i]);
         }
     }
 
     /**
      * display collection of errors to the user
-     * 
      * @return void 
      */
-    protected function displayErrors() : void
+    protected function displayErrors(): void
     {
-        for($i = 0 ; $i < count($this->errors) ; $i++)
-        {
+        for ($i = 0 ; $i < count($this->errors) ; $i++) {
             $this->line("<bg=red>".$this->errors[$i]."</>");
         }
     }
@@ -99,75 +83,75 @@ class CreateProduct extends Command
      * validate different data entered by the user
      * 
      * @param string $field
-     * 
      * @param string $value
-     * 
      * @param  array $rules
-     * 
      * @return void 
      */
-    public function validate(string $field  , string $value = null,array $rules) : array
+    public function validate(string $field, string $value = null, array $rules): array
     {
         $validator = Validator::make(
             [ $field => $value],
             [ $field => $rules]
         );
-        if($validator->fails())
-            $this->addError($field,$validator->errors()->all());
+        if ($validator->fails())
+            $this->addError($validator->errors()->all());
         $this->validData->put($field,$value);
         return [];
     }
 
     /**
      * ask user for input data.
-     * 
      * do validation
-     * 
      * @return void
      */
-    protected function askAndvalidateInput() : void 
+    protected function askAndvalidateInput(): void 
     {
         $data = $this->ask('Product\'s name');
-        $this->validate("name",$data,['required','string','unique:products']);
+        $this->validate("name", $data, ['required','string','unique:products']);
       
         
         $data = $this->ask('Describe your product ');
-        $this->validate("description",$data,['required','string','max:1000']);
+        $this->validate("description", $data, ['required','string','max:1000']);
     
     
         $data = $this->ask('Product\'s Price ');
-        $this->validate("price",$data,['required','numeric','gt:0']);
+        $this->validate("price", $data, ['required','numeric','gt:0']);
      
         
         $data = $this->ask('Product\'s Category');
-        $this->validate("category",$data,['required','exists:categories,name']);
+        $this->validate("category", $data, ['required','exists:categories,name']);
     
 
         $data = $this->ask('image url');
-        $this->validate("url",$data,['required','url']);
+        $this->validate("url", $data, ['required','url']);
     }
   
-
     /**
      * Execute the console command.
-     *
-     * @return int
+     * @return void
      */
-    public function handle()
+    public function handle(): void 
     {
         $this->askAndvalidateInput();
 
         if (count($this->errors))
-            return $this->displayErrors($this->errors);
-        /** create an uploaded image  */
-        $image = $this->cliService->getFile($this->validData['url']);
-        $file = $this->cliService->getUploadFile($image, $this->validData['name']);
-        $this->validData->put("image",$file);
+            $this->displayErrors($this->errors);
+        else {
+            /** create an uploaded image  */
+            $image = $this->cliService->getFile($this->validData['url']);
 
-        $this->validData->forget('url');
-        $categories =  $this->cliService->getCategoryService()->getCategories($this->validData['category']);
-		$product = $this->cliService->getProductService()->storeProduct($this->validData->put('categories',$categories));
+            $file = $this->cliService->getUploadFile($image, $this->validData['name']);
+            $this->validData->put("image",$file);
 
+            $this->validData->forget('url');
+            $categories = $this->cliService->getCategoryService()
+                                           ->getCategories($this->validData['category']);
+
+            $product = $this->cliService->getProductService()
+                                        ->storeProduct(
+                                            $this->validData->put('categories',$categories)
+                                        );
+            $this->line("<bg=green>Product created successfully ".$product."</>");
+        }
     }
-
 }
