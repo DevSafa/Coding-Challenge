af50d8944560964a35cf6f8584a604a405e0590f
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 15:30:41 +0100

    coding style

--- Projects/challenge/routes/web.php
+++ Projects/challenge/routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/categories',[CategoryController::class,"index"]);
 Route::post('/products',[ProductController::class,"store"]);
 Route::get('/products',[ProductController::class,"index"]);
