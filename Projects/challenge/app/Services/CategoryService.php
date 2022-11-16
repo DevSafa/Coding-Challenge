94fe761975f89a070cf33bd3378607a2c48b3677
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 15:25:44 +0100

    Coding style for services

--- Projects/challenge/app/Services/CategoryService.php
+++ Projects/challenge/app/Services/CategoryService.php
@@ -4,63 +4,63 @@ namespace App\Services;
 use App\Interfaces\CategoryServiceInterface;
 use App\Interfaces\CategoryRepositoryInterface;
 
-class CategoryService implements CategoryServiceInterface {
-	private $categoryRepository;
+class CategoryService implements CategoryServiceInterface 
+{
+    /**
+     * category repository instance 
+     * @var CategoryRepositoryInterface
+     */
+    protected $categoryRepository = null;
 
-	/**
-	 * Create a new CategoryService instance.
-	 *
-	 * @param  CategoryRepository  $categoryRepository
-	 * 
-	 * @return void
-	*/
-	public function __construct(CategoryRepositoryInterface $categoryRepository)
-	{
-		$this->categoryRepository = $categoryRepository;
-	}
-
-	/**
-	 * get all categories and their sub categories.
-	 *
-	 * @return  array
-	*/
-	public function index() : array
-	{
-		return $this->categoryRepository->index()->toArray();
-	}
-	
-	/**
-	 * get categories parents of a single category
-	 * 
-	 * @param string $name
-	 * 
-	 * @return array
-	 */
-	public function getCategories(string $name) : array
-	{
-		/** get Category id  */
-		$id = $this->categoryRepository->getCategoryId($name);
-
-		/* store categories in array */
-		$categories = array();
-		array_push($categories, $id);
-		
-		
-		$parent = $this->categoryRepository->getParent($id); 
-		while ($parent->isNotEmpty())
-		{
-			array_push($categories, $parent[0]['id']);
-			$parent = $this->categoryRepository->getParent($parent[0]->id);
-		}
-		return $categories;
-	}
-
-	public function getProducts(int $id) : array
+    /**
+     * Create a new CategoryService instance.
+     * @param  CategoryRepositoryInterface  $categoryRepository
+     * @return void
+    */
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
     {
-		return $this->categoryRepository->getProducts($id)->toArray();
-
-
+        $this->categoryRepository = $categoryRepository;
     }
 
+    /**
+     * get all categories and their sub categories.
+     * @return  array
+    */
+    public function index(): array
+    {
+        return $this->categoryRepository->index()->toArray();
+    }
 
+    /**
+     * get categories parents of a single category
+     * @param string $name
+     * @return array
+     */
+    public function getCategories(string $name): array
+    {
+        /** get Category id  */
+        $id = $this->categoryRepository->getCategoryId($name);
+
+        /* store categories in array */
+        $categories = array();
+        array_push($categories, $id);
+        
+        
+        $parent = $this->categoryRepository->getParent($id); 
+        while ($parent->isNotEmpty()) {
+            array_push($categories, $parent[0]['id']);
+            $parent = $this->categoryRepository->getParent($parent[0]->id);
+        }
+        return $categories;
+    }
+
+    /**
+     * get products of a single category
+     * @param int $id
+     * @return array 
+     */
+    public function getProducts(int $id): array
+    {
+        return $this->categoryRepository->getProducts($id)->toArray();
+    }
 }
