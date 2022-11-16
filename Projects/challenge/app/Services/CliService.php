3c9e5e866d4029c549a906093b8003b8aec2020d
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 15:23:18 +0100

    Projects/challenge/app/Services/CategoryService.php

--- Projects/challenge/app/Services/CliService.php
+++ Projects/challenge/app/Services/CliService.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Services;
 
 use App\Interfaces\ProductServiceInterface;
@@ -7,106 +6,91 @@ use App\Interfaces\CategoryServiceInterface;
 use App\Interfaces\CliServiceInterface;
 use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
-use App\Http\Requests\CreateProductRequest;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Collection;
-
 
 class CliService implements CliServiceInterface
 {
     /**
-     * a temporary file instance | file handle instance with unique id in read-write mode
-     * 
+     * a temporary file instance
+     * file handle instance with unique id in read-write mode
      * @var  resource|false
      */
     protected $tempFile;
 
     /**
-     *  a category service instance
-     * 
-     * @var App\Services\CategoryService
+     * a category service instance
+     * @var CategoryService
      */
-    protected $categoryService;
+    protected $categoryService = null;
 
 
     /**
      *  a product service instance
-     * 
-     * @var App\Services\ProductService
+     * @var ProductService
      */
-    protected $productService;
+    protected $productService = null;
 
     /**
      * create a new instance of CliService
      * 
-     * @param App\Interfaces\CategoryServiceInterface $categoryService
-     * 
-     * @param App\Interfaces\ProductServiceInterface $productService
-     * 
+     * @param CategoryServiceInterface $categoryService
+     * @param ProductServiceInterface $productService
      * @return void
      */
-    public function __construct(CategoryServiceInterface $categoryService ,
-                                ProductServiceInterface $productService)
+    public function __construct(
+        CategoryServiceInterface $categoryService, 
+        ProductServiceInterface $productService
+    )
     {
         $this->tempFile = tmpFile();
         $this->categoryService  = $categoryService;
         $this->productService  = $productService;
     }
-    
+
     /**
      * get CategoryService instance
-     * 
-     * @return App\Interfaces\CategoryServiceInterface
+     * @return CategoryServiceInterface
      */
-    public function getCategoryService() : CategoryServiceInterface
+    public function getCategoryService(): CategoryServiceInterface
     {
         return $this->categoryService;
     }
 
     /**
      * get ProductService instance
-     * 
-     * @return App\Interfaces\ProductServiceInterface
+     * @return ProductServiceInterface
      */
-    public function getProductService() : ProductServiceInterface
+    public function getProductService(): ProductServiceInterface
     {
         return $this->productService;
     }
 
     /**
      * get UploadedFile
-     * 
-     * @param Illuminate\Http\File $file
-     * 
+     * @param File $file
      * @param string $name
-     * 
-     * @return  Illuminate\Http\UploadedFile
+     * @return UploadedFile
      */
-    public function getUploadFile(File $file , string $name) : UploadedFile
+    public function getUploadFile(File $file, string $name): UploadedFile
     {
-        $uploadedFile = new UploadedFile
-            (
-                $file->getPathname(),
-                $name,
-                $file->getMimeType(),
-                $file->getSize(),
-                0,
-                false 
-            );
-        return  $uploadedFile;
+        $uploadedFile = new UploadedFile(
+            $file->getPathname(), 
+            $name, $file->getMimeType(), 
+            $file->getSize(), 
+            0, 
+            false
+        );
+        return $uploadedFile;
     }
 
     /**
      * read content of a file store it
-     * 
      * @param string $url
-     * 
-     * @return  Illuminate\Http\File
+     * @return File
      */
     public function getFile(string $url) : File
     {
-        $fileContent =  @file_get_contents($url);
-       
+        $fileContent = @file_get_contents($url);
+        
         $tempFilePath = stream_get_meta_data($this->tempFile)['uri'];
         
         file_put_contents($tempFilePath, $fileContent);
@@ -115,5 +99,4 @@ class CliService implements CliServiceInterface
 
         return $tempFileObject;
     }
-
 }
--- Projects/challenge/app/Services/ProductService.php
+++ Projects/challenge/app/Services/ProductService.php
@@ -4,115 +4,116 @@ namespace App\Services;
 
 use App\Interfaces\ProductServiceInterface;
 use App\Interfaces\ProductRepositoryInterface;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\Collection as SupportCollection;
 use Illuminate\Http\UploadedFile as Image;
 
-class ProductService implements ProductServiceInterface {
+class ProductService implements ProductServiceInterface 
+{
+    /**
+     * The product repository instance.
+     * @var ProductRepositoryInterface
+    */
+    protected $productRepository = null;
 
-	/**
-	 * The product repository instance.
-	*/
-	protected $productRepository;
+    /**
+     * SupportCollection instance.
+     * @var SupportCollection
+    */
+    protected $values = null;
 
-	/**
-	 *  Illuminate\Support\Collection instance.
-	*/
-	protected $values;
+    /**
+     * Create a new ProductService instance
+     * @param  ProductRepository  $productRepository
+     * @return void
+    */
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
 
-	/**
-	 * Create a new ProductService instance.
-	 *
-	 * @param  ProductRepository  $productRepository
-	 * 
-	 * @return void
-	*/
-	public function __construct(ProductRepositoryInterface $productRepository)
-	{
-		$this->productRepository = $productRepository;
-	}
+    /**
+     * get all products
+     * @return  array
+    */
+    public function index(): array
+    {
+        return $this->productRepository->index()->toArray();
+    }
 
-	/**
-	 * get all products
-	 *
-	 * @return  Illuminate\Database\Eloquent\Collection
-	*/
-	public function index() :array
-	{
-		return $this->productRepository->index()->toArray();
-	}
+    /**
+     * get image extension from image object from $values
+     * @return  string
+    */
+    protected function getImageExtension(): string
+    {
+        return $this->values['image']->extension();
+    }
 
-	/**
-	 * get image extension from image object from  SupportCollection instance
-	 *
-	 * @return  string
-	*/
-	protected function getImageExtension() : string
-	{
-		
-		return $this->values['image']->extension();
-	}
+    /**
+     * get image name from image object from $values
+     * @return  string
+    */
+    protected function getImageName(): string
+    {
+        return $this->values['name'];
+    }
 
-	/**
-	 * get image name from image object from  Supportcollection instance
-	 *
-	 * @return  string
-	*/
-	protected function getImageName() : string
-	{
-		return $this->values['name'];
-	}
+    /**
+     * generate a unique image name 
+     * @return  string
+    */
+    protected function generateImageName(): string
+    {
+        return uniqid()
+                . '-' 
+                .$this->getImageName()
+                . '.' 
+                . $this->getImageExtension();
+    }
 
-	/**
-	 * generate a unique image name 
-	 *
-	 * @return  string
-	*/
-	protected function generateImageName() : string
-	{
-		return uniqid() . '-' .$this->getImageName(). '.' . $this->getImageExtension();
-	}
-
-	/**
-	 * getPrice of an image from SupportCollection instance and cast it to float
-	 *
-	 * @return  float
-	*/
-	protected function getPrice() : float
-	{
-		return (float)($this->values['price']);
-	}
-
-	protected function uploadImage(Image $image, string $name) : void 
-	{
-		$image->storeAs('public/images',$name);
-	
-	}
-
-	/**
-	 * the final data to pass to database Repository
-	 * 
-	 * uploading image in storage
-	 *
-	 * @param  Illuminate\Support\Collection  $values
-	 * 
-	 * @return  void
-	*/
-	public function storeProduct(SupportCollection $values) : array
-	{	
-		$this->values = $values;
-
-		$collection = collect([
-			'name' => $values['name'],
-			'description' => $values['description'],
-			'price' => $this->getPrice(),
-			'image' =>$this->generateImageName()
-		]);
-		
-		$product = $this->productRepository->storeProduct(($collection),$values['categories']);
-		$this->uploadImage($values['image'],$collection['image']);
-		return $product;
+    /**
+     * getPrice of an image from $values cast it to float
+     * @return  float
+    */
+    protected function getPrice(): float
+    {
+        return (float)($this->values['price']);
+    }
 
 
-	}
+    /**
+     * store image 
+     * @param Image $image
+     * @param string $name
+     * @return void
+    */
+    protected function uploadImage(Image $image, string $name): void 
+    {
+        $image->storeAs('public/images',$name);
+
+    }
+
+    /**
+     * the final data to pass to product Repository
+     * uploading image in storage
+     
+     * @param  SupportCollection  $values
+     * @return  array
+    */
+    public function storeProduct(SupportCollection $values) : array
+    {	
+        $this->values = $values;
+
+        $collection = collect([
+            'name' => $values['name'],
+            'description' => $values['description'],
+            'price' => $this->getPrice(),
+            'image' =>$this->generateImageName()
+        ]);
+        
+        $product = $this->productRepository
+                    ->storeProduct(($collection), $values['categories']);
+        $this->uploadImage($values['image'], $collection['image']);
+        return $product;
+    }
 }
