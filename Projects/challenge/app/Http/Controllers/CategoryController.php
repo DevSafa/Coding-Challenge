4ed9a05c5d6cffc9db5e3b5a4cf5e5bed734da47
sbarkadev <software.dev.safa@gmail.com>
2022-11-16 14:15:36 +0100

    fix coding style for controllers

--- Projects/challenge/app/Http/Controllers/CategoryController.php
+++ Projects/challenge/app/Http/Controllers/CategoryController.php
@@ -5,38 +5,38 @@ use App\Interfaces\CategoryServiceInterface;
 
 class CategoryController extends Controller
 {
-	/**
-	 * The category service instance.
-	*/
-	protected $categoryService;
+    /**
+     * The category service instance
+     * @var CategoryServiceInterface
+    */
+    protected $categoryService = null;
 
-	/**
-	 * Create a new CategoryController instance.
-	 *
-	 * @param  CategoryService  $categoryService
-	 * 
-	 * @return void
-	*/
-	public function __construct(CategoryServiceInterface $categoryService)
-	{
-		$this->categoryService = $categoryService;
-	}
-
-	/**
-	 * Call the  @method index in CategoryService instance 
-	 * to get all categories and their sub categories
-	 *
-	 * @return  array
-	*/
-	public function index() : array
-	{
-		return $this->categoryService->index();
-	}
-
-	public function getProducts(int $id) : array
+    /**
+     * Create a new CategoryController instance
+     * @param  CategoryServiceInterface  $categoryService
+     * @return void
+    */
+    public function __construct(CategoryServiceInterface $categoryService)
     {
-		return $this->categoryService->getProducts($id);
+        $this->categoryService = $categoryService;
+    }
+
+    /**
+     * call the category service instance to get all categories
+     * @return  array
+    */
+    public function index(): array
+    {
+        return $this->categoryService->index();
+    }
+
+    /**
+     *call the category service instance to get products by category
+     * @param  int $id
+     * @return array
+     */
+    public function getProducts(int $id): array
+    {
+        return $this->categoryService->getProducts($id);
     }
 }
-
-
--- Projects/challenge/app/Http/Controllers/ProductController.php
+++ Projects/challenge/app/Http/Controllers/ProductController.php
@@ -1,70 +1,67 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateProductRequest;
 use App\Interfaces\ProductServiceInterface;
 use App\Interfaces\CategoryServiceInterface;
-use App\Interfaces\ProductCategoryServiceInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
-	/**
-	 * The productService instance.
-	*/
-	protected $productService;
+    /**
+     * The product service instance
+     * @var ProductServiceInterface
+    */
+    protected $productService = null;
 
-	/**
-	 * The categoryService instance.
-	*/
-	protected $categoryService;
+    /**
+     * The category service instance
+     * @var CategoryServiceInterface
+    */
+    protected $categoryService = null;
 
-	/**
-	 * Create a new ProductController instance.
-	 *
-	 * @param  ProductService  $productService
-	 * 
-	 * @return void
-	*/
-	public function __construct(
-			ProductServiceInterface $productService , 
-			CategoryServiceInterface $categoryService)
-	{
-		$this->productService = $productService;
-		$this->categoryService = $categoryService;
+    /**
+     * Create a new ProductController instance
+     * @param  ProductServiceInterface  $productService
+     * @param  CategoryServiceInterface $categoryService
+     * @return void
+    */
+    public function __construct(
+        ProductServiceInterface $productService, 
+        CategoryServiceInterface $categoryService
+    ) {
+        $this->productService = $productService;
+        $this->categoryService = $categoryService;
+    }
 
-	}
+    /**
+     * Call the product ervice  instance to get all products
+     * @return  array
+    */
+    public function index(): array
+    {
+        return $this->productService->index();
+    }
 
-	/**
-	 * Call the  @method index in ProductService instance to get all products
-	 * 
-	 * @return  array
-	*/
-	public function index() : array
-	{
-		return $this->productService->index();
-	}
-
-	/**
-	 * 
-	 * validate data using CreateProductRequest 
-	 * 
-	 * get data for Category and Product services  using CreateProductRequest
-	 * 
-	 * @param  App\Http\Requests\CreateProductRequest  $request
-	 * 
-	 * @return  void
-	*/
-	public function store(CreateProductRequest $request) : Response
-	{
-		$dataForCategoryService = $request->getDataForCategoryService();
-		$dataForProductService = $request->getDataForProductService();
-
-		$categories =  $this->categoryService->getCategories($dataForCategoryService);
-		$product = $this->productService->storeProduct($dataForProductService->put('categories',$categories));
-
-		return response($product , 201)
-                  ->header('Content-Type', 'application/json');
-	}
+    /**
+     * validate data using CreateProductRequest
+     * prepare data for product, category  services using CreateProductRequest
+     * @param CreateProductRequest  $request
+     * @return  void
+    */
+    public function store(CreateProductRequest $request): Response
+    {
+        $dataForCategoryService = $request->getDataForCategoryService();
+        $dataForProductService = $request->getDataForProductService();
+        
+        $categories = $this->categoryService->getCategories(
+            $dataForCategoryService
+        );
+        $product = $this->productService->storeProduct(
+            $dataForProductService->put('categories', $categories)
+        );
+        
+        return response($product , 201)
+            ->header('Content-Type', 'application/json');
+    }
 }
