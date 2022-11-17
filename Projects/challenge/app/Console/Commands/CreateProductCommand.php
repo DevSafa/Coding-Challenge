b482c8c79c423c11960637a757f933d94f3d3548
sbarkadev <software.dev.safa@gmail.com>
2022-11-17 05:36:38 +0100

    separate validation from command class

---
+++ Projects/challenge/app/Console/Commands/CreateProductCommand.php
@@ -0,0 +1,120 @@
+<?php
+namespace App\Console\Commands;
+
+use App\Console\Validation\CliValidation;
+use App\Interfaces\CliServiceInterface;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+
+class CreateProductCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'product:create';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Create a product from command line';
+
+    /**
+     * The cliService instance.
+     * 
+     * @var App\Interfaces\CliServiceInterface
+    */
+	protected $cliService = null;
+
+    /**
+     * The cliService instance.
+     * @var App\Interfaces\CliServiceInterface
+    */
+	protected $cliValidation = null;
+
+    /**
+     * Create a new command instance.
+     * @param App\Interfaces\CliServiceInterface $cliService
+     * @return void 
+     */
+    public function __construct(CliServiceInterface $cliService)
+    {
+        parent::__construct();
+        $this->cliService = $cliService;
+        $this->cliValidation = new CliValidation();
+    }
+
+    /**
+     * display collection of errors to the user
+     * @return void 
+     */
+    protected function displayErrors(Collection $errors): void
+    {
+        for ($i = 0 ; $i < count($errors) ; $i++) {
+            $this->line("<bg=red>".$errors[$i]."</>");
+        }
+    }
+
+    /**
+     * ask user for input data.
+     * do validation
+     * @return void
+     */
+    protected function askAndvalidateInput(): void 
+    {   
+        $dataToValidate = collect();
+        $this->line("<fg=yellow>Product creation will take some time...</>");
+
+        $data = $this->ask('Product\'s name');
+        $dataToValidate->put("name",$data);
+        
+        $data = $this->ask('Describe your product');
+        $dataToValidate->put("description",$data);
+
+        $data = $this->ask('Product\'s Price');
+        $dataToValidate->put("price",$data);
+
+        $data = $this->ask('Product\'s Category');
+        $dataToValidate->put("category",$data);
+
+        $data = $this->ask('image url');
+        $dataToValidate->put("url",$data);
+
+        $this->cliValidation->check($dataToValidate);
+    }
+  
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle(): void 
+    {
+        $this->askAndvalidateInput();
+
+        $errors = $this->cliValidation->getErrors();
+        $validData = $this->cliValidation->getValidData();
+
+        if (count($errors)) {
+            $this->displayErrors($errors);
+            $this->line("<fg=red>Failed to create Product</>");
+        }
+        else {
+
+            $image = $this->cliService->getFile($validData['url']);
+
+            $file = $this->cliService->getUploadFile($image, $validData['name']);
+            $validData->put("image",$file);
+
+            $validData->forget('url');
+            $categories = $this->cliService->getCategoryService()
+                                           ->getCategories($validData['category']);
+
+            $this->cliService->getProductService()
+                                        ->storeProduct(
+                                            $validData->put('categories',$categories)
+                                        );
+            $this->line("<fg=green>Product created</>");
+        }
+    }
+}
---
+++ Projects/challenge/app/Console/Validation/CliValidation.php
@@ -0,0 +1,98 @@
+<?php
+namespace App\Console\Validation;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+
+class CliValidation 
+{
+    /**
+     * a collection instance to store validated data
+     * @var Collection
+     */
+    protected $validData = null;
+
+    /**
+     * a collection instance to store errors 
+     *@var Collection
+     */
+    protected $errors = null;
+
+    /**
+     * create an instance of CliValidation
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->validData = collect();
+        $this->errors =collect();
+    }
+
+    /**
+     * get validate data
+     * @return Collection
+     */
+    public function getValidData(): Collection
+    {
+        return $this->validData;
+    }
+
+    /**
+     * get errors
+     * @return Collection
+     */
+    public function getErrors(): Collection
+    {
+        return $this->errors;
+    }
+
+    /**
+     * validate different data entered by the user
+     * 
+     * @param string $field
+     * @param string $value
+     * @param  array $rules
+     * @return void 
+     */
+    public function validate(
+        string $field, 
+        string $value = null, 
+        array $rules
+        ): array
+    {
+        $validator = Validator::make(
+            [ $field => $value],
+            [ $field => $rules]
+        );
+        if ($validator->fails())
+            $this->addError($validator->errors()->all());
+        $this->validData->put($field,$value);
+        return [];
+    }
+
+    /**
+     * push errors to Collection 
+     * @param array errors 
+     * @return void
+     */
+    public function addError(array $errors): void
+    {
+        for ($i = 0 ; $i < count($errors) ; $i++) {
+            $this->errors->push($errors[$i]);
+        }
+    }
+
+    /**
+     * do  validation to $data
+     * @param Collection $data
+     * @return void
+     */
+    public function check(Collection $data): void
+    {
+        $this->validate("name", $data['name'], ['required','string','unique:products']);
+        $this->validate("description", $data['description'], ['required','string','max:1000']);
+        $this->validate("price", $data['price'], ['required','numeric','gt:0']);
+        $this->validate("category", $data['category'], ['required','exists:categories,name']);
+        $this->validate("url", $data['url'], ['required','url']);
+    }
+}
