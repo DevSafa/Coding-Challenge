83aeea906f2ce609782aa08c9e2ef98867c16c9e
sbarkadev <software.dev.safa@gmail.com>
2022-12-01 04:27:06 +0100

    define routes

--- Projects/challenge/routes/web.php
+++ Projects/challenge/routes/web.php
@@ -10,9 +10,5 @@ Route::get('/', function () {
 Route::get('/categories', [CategoryController::class,"index"]);
 Route::get('/filter/{id}', [CategoryController::class,"filter"])
     ->whereNumber('id');
-// ->where([
-    //     'id' => implode('|', config('categories'))
-// ]);
-
 Route::get('/products', [ProductController::class,"index"]);
 Route::post('/create', [ProductController::class,"store"]);
